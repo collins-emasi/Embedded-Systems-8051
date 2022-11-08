c2ffb32123bbce30b5c5c9342f44015842fe4803
collins-emasi <emasicollins@gmail.com>
2022-11-08 09:55:29 +0300

    Initial commit

---
+++ TIMER/README.md
@@ -0,0 +1,2 @@
+# Timers in 8051 Microcontroller
+This project is aimed at familiarizing with the timer in 8051 microcontroller.
---
+++ TIMER/timer.c
@@ -0,0 +1,35 @@
+#include <reg52.h>
+
+#define 	LED_ON		0xFF
+#define 	LED_OFF		0x00
+
+void ms_delay();
+
+void main(void){
+	TMOD = 0x01;		// Timer mode 1, 16 bit mode
+	
+	while (1){
+		P1 = LED_ON;
+		ms_delay();
+		P1 = LED_OFF;
+		ms_delay();
+	}
+}
+
+/*
+	The ms_delay() funtion below generates a delay of 5ms
+*/
+void ms_delay(){
+	// Maximum count in MODE 1 is 65536
+	// To get count for 5ms delay = (5ms / 1.085us) x 1 count = 4608 counts
+	
+	TH0 = 0x0EE; 	// Give Register TH EE
+	TL0 = 0x00;		// Give Register TL 00
+	
+	TR0 = 1;		// Turning on the timer by giving logic HIGH to TR bit
+	
+	while (!TF0);	// Wait until there is an overflow in the RF overflow bit
+	
+	TR0 = 0;		// After overflow, Turn off the timer
+	TF0 = 0;		// Reset the Timer Overflow bit to 0
+}
